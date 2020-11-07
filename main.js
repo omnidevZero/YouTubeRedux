f7ee5b1be31de2146f4e839f6b1d36ee27ab6e45
the-omnipotent <omnidev0@gmail.com>
2020-11-08 00:21:05 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,164 @@
+'use strict';
+    var reduxSettingsJSON;
+    //var hideCastButton = true;
+    //var smallPlayer = false; //WIP - current style changes cause multiple issues
+    //var darkPlaylist = true;
+
+    var flags = {
+        "likesChanged":false,
+        "stylesChanged":false
+    }
+    var likesInterval;
+    var YTReduxURLPath;
+    var YTReduxURLSearch;
+
+    function getSettings(){
+        if (localStorage.getItem("reduxSettings") == null){
+            var newSettings = '{"gridItems": 6,"hideCastButton": true,"darkPlaylist": true,"smallPlayer": false, "showRawValues": true, "autoConfirm": true}';
+            localStorage.setItem("reduxSettings", newSettings);
+            reduxSettingsJSON = JSON.parse(newSettings);
+        } else {
+            reduxSettingsJSON = JSON.parse(localStorage.getItem("reduxSettings"));
+        }
+    }
+
+    function confirmIt(){
+        var confirmButton = document.querySelector('paper-dialog > yt-confirm-dialog-renderer > div:last-child > div > #confirm-button');
+        if (confirmButton != null){
+            confirmButton.click();
+            console.log('Clicked at: ' + new Date());
+                document.querySelector('video').play();
+                console.log('Unpaused at: ' + new Date());
+                document.querySelector('paper-dialog').remove();
+        }
+    }
+
+    function resumePlayback(){
+        if (!document.querySelector('ytd-miniplayer[active]') && document.querySelector('.ytp-play-button > svg > path').getAttribute('d') == 'M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z'){ //click play if it's the displayed button icon and there is no mini player
+            document.querySelector('.ytp-play-button').click();
+            console.log('Unpaused at: ' + new Date());
+        }
+    }
+
+    function changeGridWidth(){
+        if (location.pathname == "/"){
+            var retry = setInterval(function(){
+                //console.log('Change grid')
+                var styleItem = document.querySelector("#primary > ytd-rich-grid-renderer");
+                var currentStyle = styleItem.style.cssText;
+                var currentStyleArray = currentStyle.split(";");
+                var currentSettings = [];
+                currentStyleArray.pop();
+                currentStyleArray.forEach(element => {
+                    currentSettings.push(element.slice(-1));
+                });
+
+                var newSettings = currentSettings;
+                newSettings[0] = reduxSettingsJSON.gridItems + " !important"; //to override different important from css
+                for (var i = 0; i < currentStyleArray.length; i++){
+                    currentStyleArray[i] = currentStyleArray[i].slice(0,-1) + newSettings[i];
+                }
+                styleItem.style.cssText = currentStyleArray.join(";") + ";";
+                if (currentStyle != "" && currentSettings[0] == reduxSettingsJSON.gridItems + " !important"){clearInterval(retry);};
+            },100);
+        }
+    }
+
+    function addCustomStyles(){
+        if (!flags.stylesChanged){
+            var conditionalCast = reduxSettingsJSON.hideCastButton ? `/*PLAY ON TV BUTTON*/[class="ytp-button"] {display:none !important;}` : '';
+            var conditionalPlayerSize = reduxSettingsJSON.smallPlayer ? `
+/*SMALL PLAYER*/
+ytd-watch-flexy[flexy_] #player-container-outer.ytd-watch-flexy {
+max-width:calc(480px * ( 16 / 9 ));
+max-height:480px;
+}
+.html5-video-container video {
+max-width:calc(480px * ( 16 / 9 ));
+max-height:480px;
+}
+[class="ytp-chrome-bottom"] {
+width: calc(100% - (2 * 12px)) !important;
+}
+` : '';
+            var conditionalDarkPlaylist = reduxSettingsJSON.darkPlaylist ? `
+/*DARK PLAYLIST*/
+#playlist.ytd-watch-flexy {
+transform: translate(-25px, -1px);
+}
+.header.ytd-playlist-panel-renderer {
+background-color: #1a1a1a;
+}
+ytd-playlist-panel-renderer[collapsible] .title.ytd-playlist-panel-renderer {
+color: #fff;
+}
+.publisher.ytd-playlist-panel-renderer {
+color: #B8B8B8;
+}
+.playlist-items.ytd-playlist-panel-renderer {
+background-color: #222;
+}
+#video-title.ytd-playlist-panel-video-renderer {
+color: #CACACA;
+}
+#byline.ytd-playlist-panel-video-renderer {
+color: #767676;
+}
+ytd-playlist-panel-video-renderer.ytd-playlist-panel-renderer:hover:not(.dragging) {
+background-color: #525252;
+}
+ytd-playlist-panel-video-renderer[selected] {
+background-color: #3a3a3a !important;
+}
+` : '';
+            var customStyle = document.createElement("style");
+            customStyle.innerHTML = `` + conditionalCast + conditionalPlayerSize + conditionalDarkPlaylist;
+            document.head.append(customStyle);
+            flags.stylesChanged = true;
+        }
+    }
+
+    function changeLikesCounter(){
+        if (!flags.likesChanged && document.querySelectorAll('#top-level-buttons > ytd-toggle-button-renderer:first-child > a > yt-formatted-string')[0] !== undefined){
+            var changeLikesInterval = setInterval(function(){
+                var likes = document.querySelectorAll('#top-level-buttons > ytd-toggle-button-renderer:first-child > a > yt-formatted-string')[0];
+                var dislikes = document.querySelectorAll('#top-level-buttons > ytd-toggle-button-renderer:nth-child(2) > a > yt-formatted-string')[0];
+                var rawLikes = document.querySelectorAll('#info > #menu-container > ytd-sentiment-bar-renderer > paper-tooltip > #tooltip')[0].innerText.split("/")[0].trim();
+                var rawDislikes = document.querySelectorAll('#info > #menu-container > ytd-sentiment-bar-renderer > paper-tooltip > #tooltip')[0].innerText.split("/")[1].trim();
+                likes.innerText = rawLikes;
+                dislikes.innerText = rawDislikes;
+            },10);
+            setTimeout(function(){
+                flags.likesChanged = true;
+                clearInterval(changeLikesInterval);
+            },3000);
+        }
+    }
+
+    function main(){
+        //console.log('Start main')
+        getSettings();
+        changeGridWidth();
+        addCustomStyles();
+        var changeLikes = reduxSettingsJSON.showRawValues ? setInterval(changeLikesCounter,10) : '';
+        setTimeout(function(){
+            clearInterval(changeLikes);
+        },3000);
+    }
+
+    function start(){
+        main();
+        YTReduxURLPath = location.pathname;
+        YTReduxURLSearch = location.search;
+        var checkURLChange = setInterval(function(){
+            if (location.pathname != YTReduxURLPath || location.search != YTReduxURLSearch){
+                YTReduxURLPath = location.pathname;
+                YTReduxURLSearch = location.search;
+                flags.likesChanged = false;
+                main();
+            }
+        },100);
+    }
+
+    var interval = interval == undefined ? setInterval(confirmIt, 500) : undefined;
+    start();
---
+++ popup.js
@@ -0,0 +1,107 @@
+var donateButton = document.querySelector('#donate');
+var globalURL;
+var currentSettings;
+
+donateButton.onclick = function(element) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'window.open("https://paypal.me/kowpowkow");'});
+  });
+};
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  globalURL = tabs[0].url;
+  var fields = document.querySelectorAll('fieldset');
+  if (!globalURL.includes("www.youtube.com")){
+    for (var i = 0; i < fields.length; i++){
+      document.querySelectorAll('fieldset')[i].setAttribute("disabled", "")
+    }
+    document.querySelector('.outer-warning').style.display = "table";
+  }
+});
+
+var settingsElements = document.querySelectorAll('.settings:not(.slider-control)');
+for (var i = 0; i < settingsElements.length; i++){
+  document.querySelectorAll('.settings:not(.slider-control)')[i].addEventListener('change', function(){
+    saveSettings();
+  })
+}
+
+document.querySelector('input[type="range"]').addEventListener('change', function(){
+  var inputControl = document.querySelector('.slider-control');
+  inputControl.value = this.value;
+  saveSettings();
+  changeGridWidth(this.value);
+})
+
+document.querySelector('.slider-control').addEventListener('change', function(){
+  var slider = document.querySelector('input[type="range"]');
+  slider.value = this.value;
+  saveSettings();
+  changeGridWidth(this.value);
+})
+
+function saveSettings(){
+  var newSettings = {};
+  //save slider
+  newSettings[document.querySelector('input[type="range"]').name] = document.querySelector('input[type="range"]').value;
+
+  //save checkboxes
+  var itemsCheck = document.querySelectorAll('input[type="checkbox"]');
+  for (var i = 0; i < itemsCheck.length; i++){
+    newSettings[itemsCheck[i].name] = itemsCheck[i].checked;
+  }
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var storageSettings = JSON.stringify(newSettings);
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: `localStorage.setItem("reduxSettings", ${JSON.stringify(storageSettings)})`});
+    });
+}
+
+function changeGridWidth(numberOfItems){
+  if (globalURL == "https://www.youtube.com/" || globalURL == "http://www.youtube.com/"){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: `var styleItem = document.querySelector("#primary > ytd-rich-grid-renderer");
+          styleItem.style.setProperty("--ytd-rich-grid-items-per-row", ${numberOfItems}, "important")
+          `
+          });
+      });
+    }
+  }
+
+  function getSettings(){
+    if (currentSettings == null){return};
+    var itemsCheck = document.querySelectorAll('input[type="checkbox"]');
+
+    //set slider
+    document.querySelector('input[type="range"]').value = currentSettings.gridItems;
+    document.querySelector('.slider-control').value = currentSettings.gridItems;
+    //set checkboxes
+    for (var i = 0; i < itemsCheck.length; i++){
+      for (var j = 0; j < Object.keys(currentSettings).length; j++){
+        if (itemsCheck[i].name == Object.keys(currentSettings)[j]){
+          itemsCheck[i].checked = Object.values(currentSettings)[j];
+        }
+      }
+    }
+  }
+
+  //main
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var key = "reduxSettings";
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {
+          code:`localStorage["${key}"];`
+        }, function(result){
+          if (result == null || result == undefined){return};
+          currentSettings = JSON.parse(result[0]);
+          getSettings();
+        }
+      )
+  });
