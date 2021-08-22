766adb34acc1ed6b1af647dbbf048970619e7692
the-omnipotent <omnidev0@gmail.com>
2021-08-22 20:00:35 +0200

    old icons, smaller alt loader, disable changelog

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,9 +1,16 @@
 function handleInstalled(reason) {
-	chrome.tabs.create({
-		url: `./changelog.html#${reason}`
+	browser.storage.sync.get(['reduxSettings'], function(result) {
+		if (Object.keys(result).length > 0 && !result.reduxSettings.showChangelog) {
+			return;
+		} else {
+			chrome.tabs.create({
+				url: `./changelog.html#${reason}`
+			});
+		}
 	});
 }
   
-chrome.runtime.onInstalled.addListener(reason => {
+let browser = chrome || browser;
+browser.runtime.onInstalled.addListener(reason => {
 	handleInstalled(reason.reason);
 });
--- Firefox/background.js
+++ Firefox/background.js
@@ -1,9 +1,15 @@
 function handleInstalled(reason) {
-	browser.tabs.create({
-		url: `./changelog.html#${reason}`
+	browser.storage.sync.get(['reduxSettings'], function(result) {
+		if (Object.keys(result).length > 0 && !result.reduxSettings.showChangelog) {
+			return;
+		} else {
+			chrome.tabs.create({
+				url: `./changelog.html#${reason}`
+			});
+		}
 	});
 }
-  
+
 browser.runtime.onInstalled.addListener(reason => {
 	handleInstalled(reason.reason);
 });
