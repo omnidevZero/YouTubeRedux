7f874f970d93e2a8795e23435f62c1cb35e42768
SIX10 <34385243+SIX10@users.noreply.github.com>
2022-05-16 21:19:54 -0500

    Stop Changelog from showing up from browser update
    Stops bug where changelog would appear just from browser updating and not actual extension updating.

--- Firefox/background.js
+++ Firefox/background.js
@@ -2,8 +2,9 @@ function handleInstalled(reason) {
 	browser.storage.sync.get(['reduxSettings'], function(result) {
 		if (Object.keys(result).length > 0 && !result.reduxSettings.showChangelog) {
 			return;
-		} else {
-			chrome.tabs.create({
+		} 
+		else if (reason != "browser_update") {
+			browser.tabs.create({
 				url: `./changelog.html#${reason}`
 			});
 		}
